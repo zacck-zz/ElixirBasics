4d13ea23852e7b497d6a1af037f5106cca932a28
Zacck <coderv63@gmail.com>
2016-09-24 06:26:50 +0200

    add samples for maps

--- examples/datatypes.exs
+++ examples/datatypes.exs
@@ -31,4 +31,41 @@ GenServer == :GenServer
 
 #Nil is an atom
 nil == :nil
- 
+
+
+#BINARIES
+<<byte, byte ,byte >>
+
+01101000 01100101 01101100 01101100 01101111
+
+#is represeented in ELixir as this Binary:
+<<104, 101, 108, 108, 111>>
+
+#which can also be written this way:
+"hello"
+
+#STRINGS
+#this are just binaries in elixir
+"Hello, World!"
+
+"""
+This is a multiline string.
+These are used in documentation.
+"""
+
+#MAPS
+#these are a key value store
+
+#example
+episode =  %{
+  name: "Data types",
+  student: "Zacck Osiemo"
+}
+
+
+episode.name # => Data types
+episode[:student] # => "Zacck Osiemo"
+
+
+#TUPLES
+#these are collections of items
